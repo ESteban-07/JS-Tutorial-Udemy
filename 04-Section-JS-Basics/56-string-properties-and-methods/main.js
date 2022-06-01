b18dcb5fe969fcae5048e10cbf61994720b679c1
Esteban Portillo <esteban.07.elias@gmail.com>
2022-06-01 13:43:18 -0600

    class 56: string properties and methods

---
+++ 04-Section-JS-Basics/56-string-properties-and-methods/main.js
@@ -0,0 +1,19 @@
+// String properties and methods
+let text = ' Peter Jordan';
+
+// Property
+console.log(text.length);
+
+// Methods
+console.log(text.toLowerCase());
+console.log(text.toUpperCase());
+console.log(text.charAt(1));
+console.log(text.charAt(text.length - 1));
+console.log(text.indexOf('e'));
+console.log(text.lastIndexOf('e'));
+console.log(text.trim());
+console.log(text.trim().toLowerCase().startsWith('peter'));
+console.log(text.includes('eter'));
+console.log(text.slice(0, 6));
+console.log(text.slice(-3));
+console.log(text.split(''));
