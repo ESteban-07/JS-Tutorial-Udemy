c9d8f4010ff9f716632adae5c5f298de82ce2dc1
Esteban Portillo <esteban.07.elias@gmail.com>
2022-06-01 22:07:49 -0600

    class 65: ternary operator

---
+++ 04-Section-JS-Basics/65-ternary-operator/main.js
@@ -0,0 +1,19 @@
+// unary operator - typeof
+let text = 'some text';
+console.log(typeof text); // one operand
+
+// binary operator - assignment
+let number = 3;
+let number2 = 2 + 5;
+
+// ternary operator
+// condition ? (rusn if true) : (runs if false)
+const value = 1 < 0;
+
+value ? console.log('true') : console.log('false');
+
+// if (value) {
+//     console.log('value is true');
+// } else {
+//     console.log('value is false');
+// }
