96b731689238354695913b87c9d05e276385742f
Esteban Portillo <esteban.07.elias@gmail.com>
2022-06-13 12:35:48 -0600

    class 90: window & document overview

---
+++ 05-Section-DOM/90-window-&-document-overview/main.js
@@ -0,0 +1,6 @@
+// window object = browser api
+// within the window object we have the document object
+// to console properties and methods of a window object
+// we have to use: console.dir(object)
+
+console.dir(document);
