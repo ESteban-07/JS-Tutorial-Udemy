ab4df7f6d66f7ab145f72881a5cdda7f0107e6c1
Esteban Portillo <esteban.07.elias@gmail.com>
2022-06-13 12:22:40 -0600

    class 89: DOM general concepts

---
+++ 05-Section-DOM/89-DOM-general-concepts/main.js
@@ -0,0 +1,37 @@
+/*
+##### Document Object Model ####
+
+- similar to css
+
+- Select the element or group of elements that you
+want to affect
+
+- Decide the effect that you want to apply to 
+the selection
+*/
+
+// many different ways
+
+document.body.style.backgroundColor = 'blue';
+document.body.style.color = 'yellow';
+document.getElementById('btn').style.color = 'red';
+
+// assign to a variable
+
+const element = document.getElementById('element');
+// do something
+
+document.querySelector('element'); // do something
+
+// window object
+console.log(window);
+
+// returns a node object or a node list, which is an
+// arraylike object
+
+const btn = document.getElementById('btn');
+const name = btn.nodeName;
+const css = btn.style;
+console.log(btn);
+console.log(name);
+console.log(css);
