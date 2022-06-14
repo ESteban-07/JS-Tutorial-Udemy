6459eda58a87b1efbd624d4e77132f7d06373c05
Esteban Portillo <esteban.07.elias@gmail.com>
2022-06-14 13:04:01 -0600

    class 91: Get Element By Id

---
+++ 05-Section-DOM/91-get-element-by-id/main.js
@@ -0,0 +1,9 @@
+// Select the element or gruop of elements that we want
+// Decide the effect we want to apply to the selection
+
+const h1 = document.getElementById('title');
+h1.style.color = 'red';
+
+const btn = document.getElementById('btn');
+btn.style.backgroundColor = 'purple';
+btn.style.color = 'white';
