8a50973f54b541c55b0b335202b6074a3a33c60a
Esteban Portillo <esteban.07.elias@gmail.com>
2022-06-14 14:49:33 -0600

    class 96: Navigate the DOM - parentElement

---
+++ 05-Section-DOM/96-navigate-the-DOM-parentElement/main.js
@@ -0,0 +1,6 @@
+// parent Element
+// this property can return null when there is no parent element
+const heading = document.querySelector('h2');
+
+const parent = heading.parentElement;
+parent.style.color = 'red';
