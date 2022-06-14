c1c80bac364afee19cffd8699d35d5e736efe6e3
Esteban Portillo <esteban.07.elias@gmail.com>
2022-06-14 13:43:50 -0600

    class 93: Get Elements By Class Name

---
+++ 05-Section-DOM/93-get-elements-by-class-name/main.js
@@ -0,0 +1,10 @@
+// Select the element or group of elements that we want
+// Decide the effect we want to apply to the selection
+
+// document.getElementsByClassName('className');
+// HTMLCollection
+
+const listItems = document.getElementsByClassName('special');
+listItems[2].style.color = 'blue';
+
+console.log(listItems);
