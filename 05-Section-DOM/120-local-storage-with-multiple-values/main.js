0b9197c40dd17df8e82f759a5cb93744eab38419
Esteban Portillo <esteban.07.elias@gmail.com>
2022-06-27 12:54:08 -0600

    class 120: Local Storage With Multiple Values

---
+++ 05-Section-DOM/120-local-storage-with-multiple-values/main.js
@@ -0,0 +1,34 @@
+// JSON.stringfy(), JSON.parse()
+
+const friends = ['john', 'peter', 'bob'];
+
+localStorage.setItem('friends', JSON.stringify(friends));
+
+const values = JSON.parse(localStorage.getItem('friends'));
+
+console.log(values[0]);
+
+let fruits;
+
+if (localStorage.getItem('fruits')) {
+    fruits = JSON.parse(localStorage.getItem('fruits'));
+} else {
+    fruits = [];
+}
+
+console.log(fruits);
+// fruits.push('apple');
+fruits.push('orange');
+localStorage.setItem('fruits', JSON.stringify(fruits));
+
+const person = {
+    name: 'Elijah',
+    age: 18,
+    married: false,
+};
+
+localStorage.setItem('person', JSON.stringify(person));
+
+const persons = JSON.parse(localStorage.getItem('person'));
+
+console.log(persons);
