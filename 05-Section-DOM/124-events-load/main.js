c14e501e69ebbd53df867318ef4f3c721d1e0ef3
Esteban Portillo <esteban.07.elias@gmail.com>
2022-06-27 15:02:57 -0600

    class 124: Events - "load"

---
+++ 05-Section-DOM/124-events-load/main.js
@@ -0,0 +1,26 @@
+/*
+The load event if fired when the whole page has loaded, including
+all dependent resources such as stylesheets and images. This is
+in contrast to DOMContentLoaded, wich is fired as soon as the
+page DOM has been loaded, without waiting for resources to finish
+loading.
+
+window.addEventListener('load', function() {
+    // your code goes here
+});
+
+*/
+
+window.addEventListener('load', function () {
+    console.log('Load I will run second');
+    const img = document.querySelector('img');
+    console.log(img);
+    console.log(img.width);
+});
+
+window.addEventListener('DOMContentLoaded', function () {
+    console.log('DOMContentLoaded I will run first');
+    const img = document.querySelector('img');
+    console.log(img);
+    console.log(img.width);
+});
